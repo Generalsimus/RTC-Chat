f56e73a11a442e900ae8d9a7f058af1a57fa3b0e
Generalsimus <totorotomoney@gmail.com>
2022-06-05 16:37:16 +0400

    .

--- public/js/createRTCMediaStream.js
+++ public/js/createRTCMediaStream.js
@@ -21,29 +21,12 @@ export const createRTCMediaStream = async () => {
     const peerConnection = new RTCPeerConnection({
         iceServers: [
             {
-                urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'],
-            },
-            { urls: "stun:stun.l.google.com:19302" },
-            { urls: "stun:stun.services.mozilla.com" },
-            { urls: "stun:stun.stunprotocol.org:3478" },
-            { url: "stun:stun.l.google.com:19302" },
-            { url: "stun:stun.services.mozilla.com" },
-            { url: "stun:stun.stunprotocol.org:3478" },
-            {
-                urls: "stun:openrelay.metered.ca:80",
-            },
-            {
                 urls: "turn:openrelay.metered.ca:80",
                 username: "openrelayproject",
                 credential: "openrelayproject",
             },
             {
                 urls: "turn:openrelay.metered.ca:443",
-                username: "openrelayproject",
-                credential: "openrelayproject",
-            },
-            {
-                urls: "turn:openrelay.metered.ca:443?transport=tcp",
                 username: "openrelayproject",
                 credential: "openrelayproject",
             },
